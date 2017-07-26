0e96053ed0d8dd40c31297263a12e24b6e81b06c
Georgi Varbanov <georgi.varbanov@hds-group.local>
2017-07-26 10:44:07 +0300

    Add room

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -16,9 +17,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', generateMessage('admin', 'Welcome to the chat app'));
+  socket.on('join', (params, callback) => {
+    if(!isRealString(params.name) || !isRealString(params.room)){
+      callback('Name and room name are required!');
+    }
 
-  socket.broadcast.emit('newMessage', generateMessage('admin', 'New user joined'));
+    socket.join(params.room);
+
+    socket.emit('newMessage', generateMessage('admin', 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('admin', `${params.name} has joined`));
+
+    callback();
+  });
 
   socket.on('createMessage', (message, callback) => {
     io.emit('newMessage', generateMessage(message.from, message.text));
